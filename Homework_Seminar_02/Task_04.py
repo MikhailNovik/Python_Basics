e57b364d46ba0b2069208f5086f5ee3628049794
Mikhail <kepler186f501@gmail.com>
2023-04-11 17:40:03 +0300

    updated task #04

--- Homework_Seminar_02/Task_04.py
+++ Homework_Seminar_02/Task_04.py
@@ -1,15 +1,9 @@
 # Задача 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 # Сдвиньте все элементы списка на 2 позиции вправо.
-
 N = int(input('Введите кол-во элементов в списке: '))
-original_list = []
 original_list = list(range(-N, N + 1))
-length = len(original_list)
 print(f'Исходный список: {original_list}\n')
 
 shift = 2
-shifted_list = []
-for i in range(length):
-    shifted_list.append(original_list[i - shift])
-
-print(f'Cписок элементы, которого сдвинуты относительно исходного на 2 позиции вправо:\n{shifted_list}')
+shifted_list = original_list[-shift:] + original_list[:-shift]
+print(f'Список элементы, которого сдвинуты относительно исходного на {shift} позиции вправо:\n{shifted_list}')
