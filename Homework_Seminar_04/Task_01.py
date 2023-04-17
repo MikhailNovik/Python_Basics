dcdc4022850d492aa73de9f76cd92d2bfbee8c94
Mikhail <kepler186f501@gmail.com>
2023-04-17 19:08:22 +0300

    closed tasks #1, #2, #3

--- Homework_Seminar_04/Task_01.py
+++ Homework_Seminar_04/Task_01.py
@@ -1,5 +1,26 @@
-# 
+# Задача 1. Дано натуральное число N. 
+# Напишите метод, который вернёт список простых множителей числа N и количество этих множителей.
 
-# print(f'{} {}')
-# print(f'{} {}')
-# print(f'{} {}')
+def calc_prime_factorization(number):
+    prime_factors = []
+    divisor = 2
+
+    while divisor * divisor <= number:
+        if number % divisor == 0:
+            prime_factors.append(divisor)
+            number //= divisor
+        else:
+            divisor += 1
+    
+    if number > 1:
+        prime_factors.append(number)
+    
+    count = len(prime_factors)
+    
+    return prime_factors, count
+
+N = int(input('Введите натуральное число: '))
+
+prime_factors, count = calc_prime_factorization(N)
+print(f'Cписок простых множителей числа {N}: {prime_factors}')
+print(f'Количество простых множителей числа {N}: {count}')
--- Homework_Seminar_04/Task_02.py
+++ Homework_Seminar_04/Task_02.py
@@ -1,5 +1,12 @@
+# Задача 2. В первом списке находится информация об ассортименте мороженного, 
+# во втором списке - информация о том, какое мороженное есть на складе. 
+# Выведите названия того товара, который закончился.
 
+assortment_ice_cream = ['Сливочное', 'Бурёнка', 'Вафелька', 'Сладкоежка']
+storage_ice_cream = ['Сливочное', 'Вафелька', 'Сладкоежка']
+assortment_ice_cream = set(assortment_ice_cream)
+storage_ice_cream = set(storage_ice_cream)
 
-# print(f'{} {}')
-# print(f'{} {}')
-# print(f'{} {}')
+difference = assortment_ice_cream.difference(storage_ice_cream)
+
+print(f'Товар(ы) {difference} закончился(лись)')
