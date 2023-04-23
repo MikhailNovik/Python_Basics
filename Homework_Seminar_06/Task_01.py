0a922e5fab46ec6d0b1ac7153fcb48f83aba5f43
Mikhail <kepler186f501@gmail.com>
2023-04-23 18:07:08 +0300

    add dir Homework Seminar 06

---
+++ Homework_Seminar_06/Task_01.py
@@ -0,0 +1,13 @@
+
+import random
+
+# user = input(f'Введите \n'
+#              f'()\n'
+#              f'или введите "cтоп" для завершения программы\n'
+#              f': ').lower()
+
+# n = [random.randint(0, 10) for el in range(length)]
+# p = ''.join([string[random.randint(0, len(string) - 1)] for _ in range(length)])
+
+# print(f'{=}')
+# print(f'{=} {}')
