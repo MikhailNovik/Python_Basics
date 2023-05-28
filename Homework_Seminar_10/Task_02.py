db68a2c84a3743b1274caff61f48ab63d06ff7fe
Mikhail <kepler186f501@gmail.com>
2023-05-28 20:18:13 +0300

    closed tasks #1, #2, #3

--- Homework_Seminar_10/Task_02.py
+++ Homework_Seminar_10/Task_02.py
@@ -1,14 +1,15 @@
 # Задача 2. Постройте диаграмму с данными о численности студентов дневной формы обучения 
 # южного федерального округа за 2017 год.
-import random
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# user = input(f'Введите \n'
-#              f'()\n'
-#              f'или введите "cтоп" для завершения программы\n'
-#              f': ').lower()
+df = pd.read_csv('https://minobrnauki.gov.ru/upload/iblock/78d/78d77b235f7b3b14e1ac671e61435311.csv',
+                sep=';', encoding='cp1251')
+df = df.iloc[33:41, :]
+df = df[['Субъект РФ', 'Численность студентов, очная форма, человек, 2017']]
 
-# n = [random.randint(0, 10) for el in range(length)]
-# p = ''.join([string[random.randint(0, len(string) - 1)] for _ in range(length)])
+plot = sns.barplot(df, x = 'Субъект РФ', y = 'Численность студентов, очная форма, человек, 2017')
+plot.set_xticklabels(labels = df['Субъект РФ'], rotation = 90)
 
-# print(f'{=}')
-# print(f'{=} {}')
+plt.show()
--- Homework_Seminar_10/Task_03.py
+++ Homework_Seminar_10/Task_03.py
@@ -1,14 +1,20 @@
 # Задача 3. Постройте диаграмму количества студентов заочной формы обучения за 2019 год по всем регионам, 
 # в которых общее количество студентов не превышает 10000 за данный год.
-import random
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# user = input(f'Введите \n'
-#              f'()\n'
-#              f'или введите "cтоп" для завершения программы\n'
-#              f': ').lower()
+df = pd.read_csv('https://minobrnauki.gov.ru/upload/iblock/78d/78d77b235f7b3b14e1ac671e61435311.csv',
+                sep=';', encoding='cp1251')
 
-# n = [random.randint(0, 10) for el in range(length)]
-# p = ''.join([string[random.randint(0, len(string) - 1)] for _ in range(length)])
+df = df[['Субъект РФ', 'Численность студентов, всего человек, 2019', 'Численность студентов заочная форма, человек, 2019']]
 
-# print(f'{=}')
-# print(f'{=} {}')
+msk = df['Субъект РФ'].str.contains('федерация|федеральный', case=False, na=False)
+df = df.drop(df[msk].index)
+
+df = df[df['Численность студентов, всего человек, 2019'] <= 10000]
+
+plot = sns.barplot(df, x = 'Субъект РФ', y = 'Численность студентов заочная форма, человек, 2019')
+plot.set_xticklabels(labels = df['Субъект РФ'], rotation = 90)
+
+plt.show()
