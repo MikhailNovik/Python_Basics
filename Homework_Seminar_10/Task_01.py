404c880bed82642fcd1bd41fafa448b3a273952d
Mikhail <kepler186f501@gmail.com>
2023-05-28 17:54:35 +0300

    add dir Homework Seminar 10

---
+++ Homework_Seminar_10/Task_01.py
@@ -0,0 +1,15 @@
+# Задача 1. Найдите, какая область центрального федерального округа 
+# имела наибольшую численность студентов вечерней формы обучения в 2015 году.
+import random
+
+
+# user = input(f'Введите \n'
+#              f'()\n'
+#              f'или введите "cтоп" для завершения программы\n'
+#              f': ').lower()
+
+# n = [random.randint(0, 10) for el in range(length)]
+# p = ''.join([string[random.randint(0, len(string) - 1)] for _ in range(length)])
+
+# print(f'{=}')
+# print(f'{=} {}')
---
+++ Homework_Seminar_10/Task_02.py
@@ -0,0 +1,14 @@
+# Задача 2. Постройте диаграмму с данными о численности студентов дневной формы обучения 
+# южного федерального округа за 2017 год.
+import random
+
+# user = input(f'Введите \n'
+#              f'()\n'
+#              f'или введите "cтоп" для завершения программы\n'
+#              f': ').lower()
+
+# n = [random.randint(0, 10) for el in range(length)]
+# p = ''.join([string[random.randint(0, len(string) - 1)] for _ in range(length)])
+
+# print(f'{=}')
+# print(f'{=} {}')
